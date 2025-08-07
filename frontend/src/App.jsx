f976f44647fee90830f322018cf082eb8dd67248
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-08-07 09:25:37 +0530

    add: Contact Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import HomePage from './components/HomePage'
 import Portfolio from './components/Portfolio'
-// import ContactUs from './components/ContactUs'
+import ContactUs from './components/ContactUs'
 import IndustrialAutomation from './components/projects/IndustrialAutomation'
 import ControlPanelManufacture from './components/projects/ControlPanelManufacture'
 import MechanicalDesigningManufacture from './components/projects/MechanicalDesigningManufacture'
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          {/* <Route path="/contact" element={<ContactUs />} /> */}
+          <Route path="/contact" element={<ContactUs />} />
           <Route path="/industrial-automation" element={<IndustrialAutomation />} />
           <Route path="/control-panel-manufacture" element={<ControlPanelManufacture />} />
           <Route path="/mechanical-designing-manufacture" element={<MechanicalDesigningManufacture />} />
